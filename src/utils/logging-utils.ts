2d406d644158440b49461f570868041617c6caf5
Juni B <june@juniper-bender.tech>
2024-01-10 15:44:40 -0600

    Refactor debugLog and update package scripts
    Updated the debugLog function in logging-utils.ts to strictly compare the DEBUG_LOG_ACTIVE environment variable with 'true'. Also, the 'prepublishOnly' script was added to package.json to ensure the code is built before publishing.

--- src/utils/logging-utils.ts
+++ src/utils/logging-utils.ts
@@ -1,7 +1,7 @@
 import { nowDateTime} from "./time-utils";
 
 export function debugLog(action: string, message: string, error: boolean = false){
-    const debug: boolean = process.env.DEBUG_LOG_ACTIVE == 'true' ?? false;
+    const debug: boolean = process.env.DEBUG_LOG_ACTIVE === 'true';
     if(debug){
         console.log(`${nowDateTime()} | ${action} |${error ? " ERROR |" : ""} ${message}`)
     }
