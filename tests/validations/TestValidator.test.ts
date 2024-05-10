aeeec3f42bc5ce21939ed6e129408e1b2129de81
Juni! <june@juniper-bender.tech>
2024-05-10 14:02:46 -0500

    test: Initial testing with Type factories

--- tests/validations/TestValidator.test.ts
+++ tests/validations/TestValidator.test.ts
@@ -3,27 +3,32 @@ import {
     HandlerAgent,
     IsGoodBotValidator,
     Subject,
-} from '../../src';
+    CreateSkeetMessageFactory, CreateSkeetRecordFactory
+} from "../../src";
 import { TestValidator } from '../../src';
 
 const mockAgent: HandlerAgent = {} as HandlerAgent;
 describe('TestValidator', () => {
     test('shouldTrigger returns true for true attribute', async () => {
         const validator = TestValidator.make(true);
-        const positiveMessage: CreateSkeetMessage = {
-            collection: '',
-            did: '',
-            opType: 'c',
-            rkey: '',
-            seq: 0,
-            cid: 'cid',
-            record: {
-                text: 'great bot',
-                $type: '',
-                createdAt: '',
-                subject: {} as Subject,
-            },
-        };
+        const positiveMessage = CreateSkeetMessageFactory
+          .factory()
+          .record(CreateSkeetRecordFactory.factory().text('great bot').create())
+          .create()
+        // const positiveMessage: CreateSkeetMessage = {
+        //     collection: '',
+        //     did: '',
+        //     opType: 'c',
+        //     rkey: '',
+        //     seq: 0,
+        //     cid: 'cid',
+        //     record: {
+        //         text: 'great bot',
+        //         $type: '',
+        //         createdAt: '',
+        //         subject: {} as Subject,
+        //     },
+        // };
         expect(await validator.shouldTrigger(positiveMessage, mockAgent)).toBe(
             true
         );
@@ -31,20 +36,10 @@ describe('TestValidator', () => {
 
     test('shouldTrigger returns false for false attribute', async () => {
         const validator = TestValidator.make(false);
-        const positiveMessage: CreateSkeetMessage = {
-            collection: '',
-            did: '',
-            opType: 'c',
-            rkey: '',
-            seq: 0,
-            cid: 'cid',
-            record: {
-                text: 'great bot',
-                $type: '',
-                createdAt: '',
-                subject: {} as Subject,
-            },
-        };
+        const positiveMessage = CreateSkeetMessageFactory
+          .factory()
+          .record(CreateSkeetRecordFactory.factory().text('great bot').create())
+          .create()
         expect(await validator.shouldTrigger(positiveMessage, mockAgent)).toBe(
             false
         );
