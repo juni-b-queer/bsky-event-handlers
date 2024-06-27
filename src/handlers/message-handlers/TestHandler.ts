7ee4fdfaa7b8401286fa422ca0420c79e5af1dae
Juni <june@juniper-bender.tech>
2024-06-27 13:20:08 -0500

    breaking: Update Handler to Have a base abstract that the message handler extends

--- src/handlers/message-handlers/TestHandler.ts
+++ src/handlers/message-handlers/TestHandler.ts
@@ -7,8 +7,35 @@ import {
     AbstractMessageHandler,
     MessageHandler,
 } from './AbstractMessageHandler';
+import { AbstractHandler } from './AbstractHandler';
+import { AbstractAction } from '../../actions/AbstractAction';
 
-export class TestHandler extends AbstractMessageHandler {
+export class TestHandler extends AbstractHandler {
+    constructor(
+        validators: Array<AbstractValidator>,
+        actions: Array<AbstractAction | MessageHandler>,
+        handlerAgent: HandlerAgent
+    ) {
+        super(validators, actions, handlerAgent);
+        return this;
+    }
+
+    async handle(
+        handlerAgent: HandlerAgent | undefined,
+        ...args: any
+    ): Promise<void> {
+        const shouldTrigger = await this.shouldTrigger(...args);
+        if (shouldTrigger) {
+            try {
+                await this.runActions(...args);
+            } catch (exception) {
+                DebugLog.error('Message Handler', exception as string);
+            }
+        }
+    }
+}
+
+export class TestMessageHandler extends AbstractMessageHandler {
     constructor(
         validators: Array<AbstractValidator>,
         actions: Array<AbstractMessageAction | MessageHandler>,
--- tests/handlers/TestHandler.test.ts
+++ tests/handlers/TestHandler.test.ts
@@ -1,4 +1,5 @@
 import {
+    AbstractAction,
     AbstractMessageAction,
     AbstractValidator,
     CreateSkeetMessage,
@@ -8,9 +9,93 @@ import {
     JetstreamMessageFactory,
     TestHandler,
 } from '../../src';
+import { TestMessageHandler } from '../../src/handlers/message-handlers/TestHandler';
 
 describe('TestHandler', () => {
     let testHandler: TestHandler;
+    let mockedHandlerAgent: HandlerAgent;
+    let mockedValidators: AbstractValidator[];
+    let mockedActions: AbstractAction[];
+    let mockValidatorShouldTrigger: jest.Mock<any, any, any>;
+    let mockActionHandle: jest.Mock<any, any, any>;
+    let mockDebugError: jest.Mock<any, any, any>;
+
+    beforeEach(() => {
+        mockDebugError = jest.fn();
+        DebugLog.error = mockDebugError;
+        mockValidatorShouldTrigger = jest
+            .fn()
+            .mockImplementation((agent: HandlerAgent, ...args: any) => {
+                return args[0] === 1;
+            });
+        mockActionHandle = jest
+            .fn()
+            .mockImplementation((agent: HandlerAgent, ...args: any) => {
+                console.log(args);
+                if (args[1] === 1) {
+                    throw new Error('error');
+                }
+                return;
+            });
+        mockedHandlerAgent = {} as HandlerAgent;
+        mockedValidators = [
+            {
+                shouldTrigger: mockValidatorShouldTrigger,
+            } as unknown as AbstractValidator,
+        ];
+        mockedActions = [
+            {
+                handle: mockActionHandle,
+            } as unknown as AbstractMessageAction,
+        ];
+        testHandler = new TestMessageHandler(
+            mockedValidators,
+            mockedActions,
+            mockedHandlerAgent
+        );
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('Should throw error when calling make', () => {
+        expect(TestMessageHandler.make).toThrow(
+            'Method Not Implemented! Use constructor.'
+        );
+    });
+
+    describe('handle', () => {
+        it('should run actions when arg is 1', async () => {
+            await testHandler.handle(undefined, 1);
+
+            expect(mockValidatorShouldTrigger).toHaveBeenCalled();
+            expect(mockActionHandle).toHaveBeenCalledWith(
+                mockedHandlerAgent,
+                1
+            );
+        });
+
+        it('should run not actions when arg is 0', async () => {
+            await testHandler.handle(undefined, 0);
+
+            expect(mockValidatorShouldTrigger).toHaveBeenCalled();
+            expect(mockActionHandle).not.toHaveBeenCalled();
+        });
+
+        it('should debug log error when handle throws error', async () => {
+            // TODO Why isn't the spread and rest ops working here???
+            await testHandler.handle(undefined, ...[1, 1]);
+
+            expect(mockValidatorShouldTrigger).toHaveBeenCalled();
+            expect(mockActionHandle).toHaveBeenCalled();
+            expect(mockDebugError).toHaveBeenCalled();
+        });
+    });
+});
+
+describe('TestMessageHandler', () => {
+    let testMessageHandler: TestMessageHandler;
     let mockedHandlerAgent: HandlerAgent;
     let mockedValidators: AbstractValidator[];
     let mockedActions: AbstractMessageAction[];
@@ -49,7 +134,7 @@ describe('TestHandler', () => {
                 handle: mockActionHandle,
             } as unknown as AbstractMessageAction,
         ];
-        testHandler = new TestHandler(
+        testMessageHandler = new TestMessageHandler(
             mockedValidators,
             mockedActions,
             mockedHandlerAgent
@@ -61,7 +146,7 @@ describe('TestHandler', () => {
     });
 
     it('Should throw error when calling make', () => {
-        expect(TestHandler.make).toThrow(
+        expect(TestMessageHandler.make).toThrow(
             'Method Not Implemented! Use constructor.'
         );
     });
@@ -70,7 +155,7 @@ describe('TestHandler', () => {
         it('should run actions when opType is c', async () => {
             //make CreateSkeetMessage
             const message: JetstreamMessage = JetstreamMessageFactory.make();
-            await testHandler.handle(undefined, message);
+            await testMessageHandler.handle(undefined, message);
 
             expect(mockValidatorShouldTrigger).toHaveBeenCalled();
             expect(mockActionHandle).toHaveBeenCalledWith(
@@ -83,7 +168,7 @@ describe('TestHandler', () => {
             const message: JetstreamMessage = JetstreamMessageFactory.factory()
                 .isDeletion()
                 .create();
-            await testHandler.handle(undefined, message);
+            await testMessageHandler.handle(undefined, message);
 
             expect(mockValidatorShouldTrigger).toHaveBeenCalled();
             expect(mockActionHandle).not.toHaveBeenCalled();
@@ -98,7 +183,7 @@ describe('TestHandler', () => {
                 seq: 3,
                 cid: 'cid',
             };
-            await testHandler.handle(undefined, message);
+            await testMessageHandler.handle(undefined, message);
 
             expect(mockValidatorShouldTrigger).toHaveBeenCalled();
             expect(mockActionHandle).toHaveBeenCalled();
